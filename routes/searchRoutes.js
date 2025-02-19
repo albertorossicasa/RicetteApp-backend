213bf467a78e2785dc534f76726616440c384489
alberto rossi <albertorossi@etik.com>
2025-02-19 18:04:48 +0100

    Aggiunta route per la ricerca per un solo ingrediente

--- routes/searchRoutes.js
+++ routes/searchRoutes.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const axios = require('axios');
 
 router.get('/search', async (req, res) => {
-  const query = req.query.query;
+  const ingredient = req.query.ingredient;
   try {
-    const response = await axios.get(`https://api.spoonacular.com/recipes/complexSearch?query=${query}&apiKey=YOUR_SPOONACULAR_API_KEY`);
-    res.json(response.data.results);
+    const response = await axios.get(`https://api.spoonacular.com/recipes/findByIngredients?ingredients=${ingredient}&apiKey=YOUR_SPOONACULAR_API_KEY`);
+    res.json(response.data);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
